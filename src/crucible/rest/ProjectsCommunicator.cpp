9e643aec43f9822fd4bf6a8ca94979805fe5be82
Seb Ruiz <me@sebruiz.net>
2009-11-26 21:09:55 +1100

    Delete unused files

--- src/crucible/rest/ProjectsCommunicator.cpp
+++
@@ -1,9 +0,0 @@
-#include "ProjectsCommunicator.h"
-#include "../../Debug.h"
-
-ProjectsCommunicator::ProjectsCommunicator(QObject *parent)
-    : RestCommunicator(parent)
-{
-}
-
-QUrl ProjectsCommunicator::apiUrl(const QString &path) const
--- src/crucible/rest/ReviewsCommunicator.h
+++
@@ -1,9 +0,0 @@
-#ifndef REVIEWSCOMMUNICATOR_H
-#define REVIEWSCOMMUNICATOR_H
-
-#include "RestCommunicator.h"
-
-#include <QUrl>
-
-
-#endif // REVIEWSCOMMUNICATOR_H
